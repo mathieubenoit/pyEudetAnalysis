e29ee0c45b6dcd11bc0c86ab9903d0bc86da7fcb
mbenoit <mbenoit@4525493e-7705-40b1-a816-d608a930855b>
2014-04-22 12:29:27 +0000

    maintenance

---
+++ Track.py
@@ -0,0 +1,48 @@
+###############################################################################################################################
+#
+#        Class for the tracks and their properties
+#
+###############################################################################################################################
+from Constant import *
+
+class Track :
+
+    trackX=[]
+    trackY=[]
+    chi2 = []
+    event=0
+    ndof = []
+    iden = []
+    trackNum = []
+    dxdz = []
+    dydz = []
+    cluster=-11
+
+    def __init__(self):
+        self.trackX=[]
+        self.trackY=[]
+        self.chi2= []
+        self.event=0
+        self.ndof= []
+        self.iden=[]
+        self.trackNum=[]
+
+    def Fill(self,x,y,chi2,event,ndof,iden,trackNum):
+        self.trackX=x - pitchX/2.
+        self.trackY=y - pitchY/2.
+        self.chi2=chi2
+        self.event=event
+        self.ndof=ndof
+        self.iden=iden
+        self.trackNum=trackNum
+
+    def FindCluster(self,clusters) :
+        resX_tmp = []
+        resY_tmp = []
+
+    def Print(self):
+
+        print "##### Track #####"
+        for i,x in enumerate(self.trackX) :
+            print "Track X : %f Track Y : %f iden : %i cluster = %i"%(self.trackX[i],self.trackY[i],self.iden[i],self.cluster)
+        print "#################"
