d5a067c0c0cd8de4f155f20652e52ba42b41372e
mbenoit <mbenoit@4525493e-7705-40b1-a816-d608a930855b>
2014-04-22 12:23:53 +0000

    copying to sophie folder

---
+++ setup_CERN.sh
@@ -0,0 +1,25 @@
+export PATH="/afs/cern.ch/sw/lcg/external/Python/2.6.5/x86_64-slc5-gcc43-opt/bin:$PATH"
+
+export LD_LIBRARY_PATH="/afs/cern.ch/sw/lcg/external/Python/2.6.5/x86_64-slc5-gcc43-opt/lib:$LD_LIBRARY_PATH"
+
+#export PYTHONPATH="/afs/cern.ch/sw/lcg/external/pyanalysis/1.3_python2.6/x86_64-slc5-gcc43-opt/lib/python2.6/site-packages:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/pytools/numpy/lib/python2.6/site-packages:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/pytools/scipy/lib/python2.6/site-packages:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/pytools/sympy/lib/python2.6/site-packages:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/pytools/matplotlib/lib/python2.6/site-packages:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/pytools/guppy/lib/python2.6/site-packages:$PYTHONPATH"
+export PYTHONPATH="/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/pytools/Cython/lib/python2.6/site-packages:$PYTHONPATH"
+
+#BLAS and LAPACK
+export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/blas/20110419/x86_64-slc5-gcc43-opt/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/lapack/3.4.0/x86_64-slc5-gcc43-opt/lib:$LD_LIBRARY_PATH
+
+. /afs/cern.ch/sw/lcg/external/gcc/4.3.2/x86_64-slc5/setup.sh
+. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.33.02/x86_64-slc5-gcc43-opt/root/bin/thisroot.sh
+#. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc46-opt/root/bin/thisroot.sh
+
+
+export TBTRACK=/afs/cern.ch/eng/clic/data/DESY_TB_DATA_19-30-08-2013_results/tbtrack
+export ALIGNMENT=/afs/cern.ch/eng/clic/data/DESY_TB_DATA_19-30-08-2013_results/alignements
+export PYEUDET=/afs/cern.ch/eng/clic/data/DESY_TB_DATA_19-30-08-2013_results/pyEudetNtuples
+export EUDETPLOTS=/afs/cern.ch/eng/clic/data/DESY_TB_DATA_19-30-08-2013_results/pyEudetAnalysis_plots/Reprocessing_23_09_2013
