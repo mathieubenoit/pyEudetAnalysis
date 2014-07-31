cfc90420d6fddae2f187e756f66041495d323d30
Sophie Redford <sophie.redford@gmail.com>
2014-07-31 11:29:35 +0200

    To avoid a crash in TotalSigmaFunctionX I added a check that the histogram being fitted has GetEffectiveEntries() > 10. This avoids a fit to no data, which will crash when you try to get a fitted parameter. It could be that there is a mistake in the code leading to the histogram being empty in the first place.

--- ToolBox.py
+++ ToolBox.py
@@ -784,11 +784,15 @@ def TotalSigmaFunctionX(sigmaCharge_tmp_X,sigmaCharge_tmp_Y,dataSet,skip,dut=6):
 #                            resY_cs[i-1].Fill(aCluster.resY)
 
     g2 = TF1("m1","gaus",-0.03,0.03)
-
-    rX = tmpx.Fit(g2,"RS","")
-    sigmaResX = rX.Parameter(2)
-    sigmaResX_err = rX.ParError(2)
-    print "resolution = %f for sigma=%f"%(sigmaResX,sigmaCharge_tmp_X)
+    print "entries", tmpx.GetEntries(), tmpx.GetBinContent(0), tmpx.GetBinContent(301), tmpx.GetEffectiveEntries()
+    if tmpx.GetEffectiveEntries() > 10.:
+        rX = tmpx.Fit(g2,"RS","")
+        sigmaResX = rX.Parameter(2)
+        sigmaResX_err = rX.ParError(2)
+        print "resolution = %f for sigma=%f"%(sigmaResX,sigmaCharge_tmp_X)
+    else:
+        print "not enough events to fit."
+        sigmaResX = 9.
     return sigmaResX
 
 
