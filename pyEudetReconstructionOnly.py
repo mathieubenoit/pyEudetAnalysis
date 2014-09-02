fe5b53234b61353094bd139b133ac30ed1c00fe8
Sophie Redford <sophie.redford@gmail.com>
2014-09-02 13:53:02 +0200

    Added check to pyEudetReconstructionOnly.py to not recluster repeated pixel maps.
    Instead it just adds the clusters it found last time.
    This makes the event loop run about twice as fast, for identical results.

--- pyEudetReconstructionOnly.py
+++ pyEudetReconstructionOnly.py
@@ -166,18 +166,31 @@ canfreq.SetLogy()
 histo_freq.Draw("")
 canfreq.SaveAs("%s/Run%i/Firing_frequency_run%06i.root"%(PlotPath,RunNumber,RunNumber))
 
+
 n_matched = 0
 n_matched_edge = 0
-
-last_time=time.time()
-
-print alignment_constants
-
+last_time = time.time()
 distances_histo = TH1F("distances_histo","",100,0.0,1.0)
+prev_pixel_xhits = []
 
 for i in range(0,n_proc) :
     aDataSet.getEvent(i)
-    aDataSet.ClusterEvent(i,method_name,0.003)
+
+    # is this a new pixel map?
+    npixels_hit = len(aDataSet.p_col)
+    pixel_x_hits = []
+    for k in xrange(npixels_hit):
+        pixel_x_hits.append(aDataSet.p_col[k])
+
+    if (pixel_x_hits == prev_pixel_xhits):
+        # same pixel map as before, will add clusters already computed
+        aDataSet.AllClusters.append(clusters_tmp)
+    else:
+        # this is a new event, will cluster
+        etacorr_sigma = 0.003
+        aDataSet.ClusterEvent(i, method_name, etacorr_sigma)
+        clusters_tmp = aDataSet.AllClusters[i]
+    prev_pixel_xhits = pixel_x_hits
 
     aDataSet.GetTrack(i)
 
